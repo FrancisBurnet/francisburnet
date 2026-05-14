cb717bae0cfabc5b6f421ea808bbc70a6c96556a
FRANCIS BURNET <francisburnet@gmail.com>
2026-05-13 23:21:33 -0400

    Session 10: host TM model locally (session-10-tm), switch demo off TM CDN

--- web/includes/capstones/capstone-session-10-content.php
+++ web/includes/capstones/capstone-session-10-content.php
@@ -108,7 +108,7 @@ require __DIR__ . '/session-custom-renderer.php';
 $teachableMachineProjectUrl = 'https://teachablemachine.withgoogle.com/train/image';
 $teachableMachineGuideUrl = 'https://teachablemachine.withgoogle.com/';
 $teachableMachineDemoUrl = '/assets/demos/session-10-maskdetector.html';
-$teachableMachineHostedModelUrl = 'https://teachablemachine.withgoogle.com/models/pYkaS3Vbs/';
+$teachableMachineHostedModelUrl = '/assets/models/session-10-tm/';
 $capstoneModelUrl = '/assets/models/session-10-capstone-resnet50/';
 $teachableMachineDemoEmbedUrl = $teachableMachineDemoUrl . '?' . http_build_query([
     'engine' => 'tm',
